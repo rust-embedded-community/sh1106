2de0966a9f7d5bb3e1ab6003d18e3dd1dc855a11
James Waples <james@wapl.es>
2019-02-11 18:40:03 +0000

    Add pixelsquare_i2c example

---
+++ examples/pixelsquare_i2c.rs
@@ -0,0 +1,108 @@
+//! Draw a 1 bit per pixel black and white image. On a 128x64 SSD1306 display over I2C.
+//!
+//! Image was created with ImageMagick:
+//!
+//! ```bash
+//! convert rust.png -depth 1 gray:rust.raw
+//! ```
+//!
+//! This example is for the STM32F103 "Blue Pill" board using I2C1.
+//!
+//! Wiring connections are as follows for a CRIUS-branded display:
+//!
+//! ```
+//!      Display -> Blue Pill
+//! (black)  GND -> GND
+//! (red)    +5V -> VCC
+//! (yellow) SDA -> PB9
+//! (green)  SCL -> PB8
+//! ```
+//!
+//! Run on a Blue Pill with `cargo run --example image_i2c`, currently only works on nightly.
+
+#![no_std]
+#![no_main]
+
+extern crate cortex_m;
+extern crate cortex_m_rt as rt;
+extern crate panic_semihosting;
+extern crate stm32f1xx_hal as hal;
+
+use cortex_m_rt::ExceptionFrame;
+use cortex_m_rt::{entry, exception};
+use hal::i2c::{BlockingI2c, DutyCycle, Mode};
+use hal::prelude::*;
+use hal::stm32;
+use sh1106::prelude::*;
+use sh1106::Builder;
+
+#[entry]
+fn main() -> ! {
+    let dp = stm32::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 400_000,
+            duty_cycle: DutyCycle::Ratio2to1,
+        },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
+
+    let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+
+    disp.init().unwrap();
+    disp.flush().unwrap();
+
+    // Top side
+    disp.set_pixel(0, 0, 1);
+    disp.set_pixel(1, 0, 1);
+    disp.set_pixel(2, 0, 1);
+    disp.set_pixel(3, 0, 1);
+
+    // Right side
+    disp.set_pixel(3, 0, 1);
+    disp.set_pixel(3, 1, 1);
+    disp.set_pixel(3, 2, 1);
+    disp.set_pixel(3, 3, 1);
+
+    // Bottom side
+    disp.set_pixel(0, 3, 1);
+    disp.set_pixel(1, 3, 1);
+    disp.set_pixel(2, 3, 1);
+    disp.set_pixel(3, 3, 1);
+
+    // Left side
+    disp.set_pixel(0, 0, 1);
+    disp.set_pixel(0, 1, 1);
+    disp.set_pixel(0, 2, 1);
+    disp.set_pixel(0, 3, 1);
+
+    disp.flush().unwrap();
+
+    loop {}
+}
+
+#[exception]
+fn HardFault(ef: &ExceptionFrame) -> ! {
+    panic!("{:#?}", ef);
+}
