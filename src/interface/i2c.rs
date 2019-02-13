956829338db839c25688c22d470aacb0649490d6
James Waples <james@wapl.es>
2019-02-13 14:23:56 +0000

    Messy, partially working output

--- src/interface/i2c.rs
+++ src/interface/i2c.rs
@@ -26,38 +26,66 @@ where
     I2C: hal::blocking::i2c::Write,
 {
     fn send_commands(&mut self, cmds: &[u8]) -> Result<(), ()> {
-        // Copy over given commands to new aray to prefix with command identifier
-        let mut writebuf: [u8; 8] = [0; 8];
-        writebuf[1..=cmds.len()].copy_from_slice(&cmds[0..cmds.len()]);
+        // // Copy over given commands to new aray to prefix with command identifier
+        // let mut writebuf: [u8; 8] = [0; 8];
+        // writebuf[1..=cmds.len()].copy_from_slice(&cmds[0..cmds.len()]);
 
-        self.i2c
-            .write(self.addr, &writebuf[..=cmds.len()])
-            .map_err(|_| ())?;
+        // self.i2c
+        //     .write(self.addr, &writebuf[..=cmds.len()])
+        //     .map_err(|_| ())?;
+
+        for cmd in cmds {
+            self.i2c.write(self.addr, &[0x00, *cmd]).map_err(|_| ())?;
+        }
 
         Ok(())
     }
 
     fn send_data(&mut self, buf: &[u8]) -> Result<(), ()> {
+        const CHUNKLEN: usize = 128;
+
+        // 4 control bytes
+        const BUFLEN: usize = CHUNKLEN + 1;
+
+        // TODO: Use screen width var instead of const
+
         // Noop if the data buffer is empty
         if buf.is_empty() {
             return Ok(());
         }
 
-        let mut writebuf: [u8; 17] = [0; 17];
+        // TODO: Use commands. Page command starts at 0xb0, gets incremented for each page
+        let mut page = 0xb0;
 
-        // Data mode
-        // 8.1.5.2 5) b) in the datasheet
-        writebuf[0] = 0x40;
+        // Display width plus 4 start bytes
+        let mut writebuf: [u8; BUFLEN] = [0; BUFLEN];
 
-        for chunk in buf.chunks(16) {
-            let chunklen = chunk.len();
+        writebuf[0] = 0x40; // Data
+                            // writebuf[1] = page; // Page address
+                            // writebuf[2] = 0x02; // Lower column address
+                            // writebuf[3] = 0x10; // Upper column address (always zero, base is 10h)
 
-            // Copy over all data from buffer, leaving the data command byte intact
-            writebuf[1..=chunklen].copy_from_slice(&chunk[0..chunklen]);
+        for chunk in buf.chunks(CHUNKLEN) {
+            writebuf[1] = page;
+
+            // Copy over all data from buffer, leaving the data command bytes intact
+            writebuf[1..BUFLEN].copy_from_slice(&chunk);
 
             self.i2c
-                .write(self.addr, &writebuf[..=chunklen])
+                .write(
+                    self.addr,
+                    &[
+                        0x00, // Command
+                        page, // Page address
+                        0x02, // Lower column address
+                        0x10, // Upper column address (always zero, base is 10h)
+                    ],
+                )
                 .map_err(|_| ())?;
+
+            self.i2c.write(self.addr, &writebuf).map_err(|_| ())?;
+
+            page += 1;
         }
 
         Ok(())
