3c4b265b3b54a2936f2377c21c24eaa2f21d3732
James Waples <james@wapl.es>
2019-05-10 10:17:49 +0100

    Fix some homepage doc links

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 //! sh1106 OLED display driver
 //!
-//! The driver must be initialised by passing an I2C or SPI interface peripheral to the [`Builder`],
+//! The driver must be initialised by passing an I2C or SPI interface peripheral to the
+//! [`Builder`](builder/struct.Builder.html),
 //! which will in turn create a driver instance in a particular mode. By default, the builder
-//! returns a [`mode::RawMode`] instance which isn't very useful by itself. You can coerce the driver
+//! returns a `mode::RawMode` instance which isn't very useful by itself. You can coerce the driver
 //! into a more useful mode by calling `into()` and defining the type you want to coerce to. For
-//! example, to initialise the display with an I2C interface and [mode::GraphicsMode], you would do
-//! something like this:
+//! example, to initialise the display with an I2C interface and
+//! [`mode::GraphicsMode`](mode/graphics/struct.GraphicsMode.html), you would do something like
+//! this:
 //!
 //! ```rust,ignore
 //! let i2c = I2c::i2c1(/* snip */);
