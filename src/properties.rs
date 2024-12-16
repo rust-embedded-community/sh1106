c34f15e3cfaa3e923a0fbe828b8576ef299f86c0
HenryLiang <lzh@axiarz.com>
2024-12-16 09:46:47 +0800

    Let the i2c support be released, so that other devices on this i2C can be called

--- src/properties.rs
+++ src/properties.rs
@@ -167,4 +167,9 @@ where
     pub fn set_contrast(&mut self, contrast: u8) -> Result<(), DI::Error> {
         Command::Contrast(contrast).send(&mut self.iface)
     }
+
+    /// Release iface
+    pub fn release(self) -> DI {
+        self.iface
+    }
 }
